08b8d9f627f7ab97221be022944e1a0c33d06139
Natdanai <88823702+corodimas@users.noreply.github.com>
2024-02-26 23:40:37 +0700

    test

--- server/index.js
+++ server/index.js
@@ -41,7 +41,7 @@ app.use((req, res, next) => {
 
 app.use(
   cors({
-    origin: "ephemeral-choux-101ef6.netlify.app",
+    origin: "https://ephemeral-choux-101ef6.netlify.app",
     credentials: true
   })
 );
